96aeff2ed9a1bb169b4eca5ac3460c7161d14f84
Keith Duncan <keith@realmacsoftware.com>
2013-02-17 14:41:11 +0000

    Initial commit

--- HTTP Server/main.m
+++ HTTP Server/main.m
@@ -8,7 +8,56 @@
 
 #import <Cocoa/Cocoa.h>
 
-int main(int argc, char *argv[])
+#import "CoreNetworking/CoreNetworking.h"
+
+@interface HelloWorldRenderer : NSObject
+
+@end
+
+@implementation HelloWorldRenderer
+
++ (CFHTTPMessageRef)networkServer:(AFHTTPServer *)server renderResourceForRequest:(CFHTTPMessageRef)request
 {
-	return NSApplicationMain(argc, (const char **)argv);
+	CFHTTPMessageRef response = CFHTTPMessageCreateResponse(kCFAllocatorDefault, AFHTTPStatusCodeOK, NULL, kCFHTTPVersion1_1);
+	
+	CFHTTPMessageSetHeaderFieldValue(response, (__bridge CFStringRef)AFHTTPMessageContentTypeHeader, (__bridge CFStringRef)@"text/plain; charset=utf-8");
+	CFHTTPMessageSetBody(response, (__bridge CFDataRef)[@"Hello, World!" dataUsingEncoding:NSUTF8StringEncoding]);
+	
+	return (__bridge CFHTTPMessageRef)CFBridgingRelease(response);
+}
+
+@end
+
+static AFHTTPServer *StartHTTPServer(AFNetworkSchedule *schedule)
+{
+	AFHTTPServer *server = [AFHTTPServer server];
+	server.schedule = schedule;
+	
+	server.renderers = @[
+		[HelloWorldRenderer class],
+	];
+	
+	BOOL openSockets = [server openInternetSocketsWithSocketSignature:AFNetworkSocketSignatureInternetTCP scope:AFNetworkInternetSocketScopeLocalOnly port:8080 errorHandler:nil];
+	NSCParameterAssert(openSockets);
+	
+	return server;
+}
+
+static int server_main(void)
+{
+	AFNetworkSchedule *schedule = [[AFNetworkSchedule alloc] init];
+	[schedule scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+	
+	__unused AFNetworkServer *server = StartHTTPServer(schedule);
+	
+	[[NSRunLoop currentRunLoop] run];
+	return -1;
+}
+
+int main(int argc, char const **argv)
+{
+	@autoreleasepool {
+		return server_main();
+	}
+	return 0;
 }
